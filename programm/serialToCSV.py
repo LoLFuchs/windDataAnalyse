482b430a7d1c053f242181ecdf08c499011790a9
Your Name <you@example.com>
2025-02-03 14:37:03 +0100

    added serial reading

---
+++ programm/serialToCSV.py
@@ -0,0 +1,12 @@
+import serial
+
+def readserial(comport, baudrate):
+
+    ser = serial.Serial(comport, baudrate, timeout=0.1)         # 1/timeout is the frequency at which the port is read
+
+    while True:
+        data = ser.readline().decode().strip()
+        if data:
+            print(data)
+
+readserial("/dev/ttyACM0", 115200)
