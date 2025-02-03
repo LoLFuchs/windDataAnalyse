31b9d43a9d155cdb05158da333dc2f16286336f5
Your Name <you@example.com>
2025-02-03 14:48:04 +0100

    first data

--- programm/serialToCSV.py
+++ programm/serialToCSV.py
@@ -1,12 +1,22 @@
-import serial
+import serial, time
 
 def readserial(comport, baudrate):
 
+    start = time.time_ns()
+    f = open("Korrekt1.csv", "w")
+    f.write("Zeit in ms, Spannung in Einheitslos\n")
     ser = serial.Serial(comport, baudrate, timeout=0.1)         # 1/timeout is the frequency at which the port is read
 
     while True:
-        data = ser.readline().decode().strip()
-        if data:
-            print(data)
+        try:
+            data = ser.readline().decode().strip()
+            if data:
+                print(data)
+                line = "" + str(time.time_ns() - start) + "," + data + "\n"
+                f.write(line)
+        except(UnicodeDecodeError):
+            print("Failed")
+
+    f.close()
 
 readserial("/dev/ttyACM0", 115200)
