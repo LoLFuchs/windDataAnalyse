27a4dce616a3fadfabbf2165f9d1be8bf4f4b5b4
Your Name <you@example.com>
2025-02-03 15:13:15 +0100

    done

--- programm/serialToCSV.py
+++ programm/serialToCSV.py
@@ -1,9 +1,33 @@
 import serial, time
+from collections import deque
 
-def readserial(comport, baudrate):
+def maintain_n_values(n):
+    # Deque for maintaining the last n values
+    values = deque(maxlen=n)
+    
+    def add_value(new_value):
+        # Append the new value and the deque automatically discards the oldest if necessary
+        values.append(new_value)
+        return list(values)  # Return the current list of n values
+
+    return add_value
+
+def moving_average(values):
+    """
+    Berechnet das durchschnittliche Mittel aller Werte in der Liste.
+    
+    :param values: Eine Liste von Zahlen, aus denen das durchschnittliche Mittel berechnet werden soll.
+    :return: Der durchschnittliche Mittelwert aller Werte in der Liste.
+    """
+    if not values:
+        raise ValueError("Die Liste darf nicht leer sein.")
+    
+    return sum(values) / len(values)
+
+def readserial(comport, baudrate, n):
 
     start = time.time_ns()
-    f = open("Foehn1.csv", "w")
+    f = open("../data/Foehn3.csv", "w")
     f.write("Zeit in ms, Spannung in Einheitslos\n")
     ser = serial.Serial(comport, baudrate, timeout=0.1)         # 1/timeout is the frequency at which the port is read
 
@@ -12,6 +36,10 @@ def readserial(comport, baudrate):
             data = ser.readline().decode().strip()
             if data:
                 print(data)
+                slope = int(data) - moving_average(add_value(int(data)))
+                if abs(slope) > 150:
+                    print("ANOMALIE!!!")
+                    f.write("ANOMALIE!!!\n")
                 line = "" + str(time.time_ns() - start) + "," + data + "\n"
                 f.write(line)
         except(UnicodeDecodeError):
@@ -19,4 +47,7 @@ def readserial(comport, baudrate):
 
     f.close()
 
-readserial("/dev/ttyACM0", 115200)
+n = 100
+add_value = maintain_n_values(n)
+
+readserial("/dev/ttyACM0", 9600, n)
--- programm/windMesserArduino/windMesserArduino.ino
+++ programm/windMesserArduino/windMesserArduino.ino
@@ -5,7 +5,7 @@
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
   pinMode(LED_GROUND, OUTPUT);
   pinMode(LED_PLUS, OUTPUT);
   digitalWrite(LED_GROUND, LOW);
