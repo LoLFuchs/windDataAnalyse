dea8ea52059f1601b98d8a321ad3b74a2d2b9940
Your Name <you@example.com>
2025-02-04 11:01:40 +0100

    sius

--- data/datenVerbessern.py
+++ data/datenVerbessern.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit
 
-def sin_fn(x, A,B,C,D,E,F):
-    return A*x**2 + B*x *  C* np.sin(D*x**2 +E*x +F)
+def sin_fn(x, A,B,C,D,E):
+    return A*x**2 + B*x *  C* np.sin(D*x +E)
 
 
 
@@ -35,15 +35,18 @@ x_fine_norm = np.linspace(0, len(norm), 100)
 umwucht = to_array("Unwucht.csv")
 umwucht = umwucht[1][1000:-1000]
 x_fine_umwucht = np.linspace(0, len(norm), 100)
-umwucht_sin_param = curve_fit(sin_fn,  len(umwucht), umwucht)
-y_sin_umwucht = sin_fn(x_fine_umwucht, umwucht_sin_param[0], umwucht_sin_param[1], umwucht_sin_param[2], umwucht_sin_param[3], umwucht_sin_param[4], umwucht_sin_param[5])
+umwucht_sin_param = curve_fit(sin_fn,  len(umwucht), umwucht, p0=[0,1,1,1,0])
+ab_A,ab_B,ab_C,ab_D,ab_E = umwucht_sin_param[0]
+y_sin_umwucht = sin_fn(x_fine_umwucht, ab_A,ab_B,ab_C,ab_D,ab_E)
 #x_umwucht = np.linspace(min(umwucht[0]), max(umwucht[1]), 100)
 #koef_umwucht = np.polyfit(umwucht[0], umwucht[1], 2)
 #poly_umwucht = np.poly1d(koef_umwucht)
 #y_poly_umwucht = poly_umwucht(x_umwucht)
 
-norm_sin_param, _ = curve_fit(sin_fn, len(norm), norm)
-y_sin_norm = sin_fn(x_fine_norm, norm_sin_param[0], norm_sin_param[1], norm_sin_param[2], norm_sin_param[3], norm_sin_param[4], norm_sin_param[5])
+norm_sin_param = curve_fit(sin_fn, len(norm), norm, p0=[0,1,1,1,0])
+print(norm_sin_param)
+ab_A,ab_B,ab_C,ab_D,ab_E =norm_sin_param[0]
+y_sin_norm = sin_fn(x_fine_norm, ab_A ,ab_B,ab_C,ab_D,ab_E)
 
 
 #y_fine = np.interp(x_fine, x, y, 100)
